259ae5c832a343addaf48427c0264ae90bdd0892
amaroto <nirk.maroto@gmail.com>
2023-02-13 21:47:43 +0100

    feat: sequelize configuration and migrations

---
+++ src/database/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: '',
+    database: 'heroApi',
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  }
+};
