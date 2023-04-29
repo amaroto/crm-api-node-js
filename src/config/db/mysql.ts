426199eb4d19062869e513b1f432bef58e26020c
amaroto <nirk.maroto@gmail.com>
2023-04-29 10:32:33 +0200

    fix: config sequelize with env variables

--- src/config/db/mysql.ts
+++ src/config/db/mysql.ts
@@ -1,15 +1,20 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const dbInit = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
-  host: `${process.env.DB_URI}`,
-  dialect: "mysql",
-});
+const dbInit = new Sequelize(
+  `${process.env.DB_NAME}`,
+  `${process.env.DB_USER}`,
+  `${process.env.DB_PASSWORD}`,
+  {
+    host: `${process.env.DB_HOST}`,
+    dialect: `${process.env.DB_DIALECT}`,
+  }
+);
 
 try {
   dbInit.authenticate();
-  console.log('Connection with database has been established successfully.');
+  console.log("Connection with database has been established successfully.");
 } catch (error) {
-  console.error('Unable to connect to the database:', error);
+  console.error("Unable to connect to the database:", error);
 }
 
 export default dbInit;
